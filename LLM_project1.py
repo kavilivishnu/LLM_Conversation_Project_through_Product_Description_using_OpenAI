9427b99aa54819cd6f6c0787e3b03673355d3485
Kavili Sri Vishnu Vardhan <68709047+kavilivishnu@users.noreply.github.com>
2023-12-12 16:05:36 +0530

    Create LLM_project1.py

---
+++ LLM_project1.py
@@ -0,0 +1,5 @@
+view = f"Click on this - {https://colab.research.google.com/drive/17ZZtcqgf221IeXoGQHDQY4jH6oDqdbdF?usp=sharing} link to view this project"
+
+permission = "Request for access, if you want to view the contents of this project"
+
+greetings = "Thank you very much for visiting my Repository"
